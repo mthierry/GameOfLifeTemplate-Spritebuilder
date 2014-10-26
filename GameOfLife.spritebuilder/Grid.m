4b4966fc12ec9a05208911cff81727f70fb95b9b
Michel Thierry <github@michelthierry.org>
2014-10-26 19:00:49 +0000

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Grid.m
+++ GameOfLife.spritebuilder/Grid.m
@@ -160,14 +160,17 @@ static const int GRID_COLUMNS = 10;
         {
             // access the creature in the cell that corresponds to the current row/column
             Creature *currentCreature = _gridArray[i][j];
-            if (currentCreature.isAlive)
-                numAlive++;
             
-            if (currentCreature.livingNeighbors == 3)
+            if (currentCreature.livingNeighbors == 3) {
                 currentCreature.isAlive = YES;
+                numAlive++;
+            }
             else if (currentCreature.livingNeighbors <= 1 ||
-                     currentCreature.livingNeighbors >= 4)
+                     currentCreature.livingNeighbors >= 4) {
+                if (currentCreature.isAlive)
+                    numAlive--;
                 currentCreature.isAlive = NO;
+            }
         }
     }
     
