cf8f73afd0808c722c94c502be71a0ae9f3d5f20
Michel Thierry <github@michelthierry.org>
2014-10-26 18:18:09 +0000

    Automatically Committed for MakeGamesWithUs

---
+++ GameOfLife.spritebuilder/Grid.h
@@ -0,0 +1,16 @@
+//
+//  Grid.h
+//  GameOfLife
+//
+//  Created by Michel Thierry on 2610//14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Grid : CCSprite
+
+@property (nonatomic, assign) int totalAlive;
+@property (nonatomic, assign) int generation;
+
+@end
