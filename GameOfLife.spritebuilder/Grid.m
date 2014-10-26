6f3b2ae2ffff6a84fa57d498bfc09ca5179209e7
Michel Thierry <github@michelthierry.org>
2014-10-26 18:46:11 +0000

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Grid.m
+++ GameOfLife.spritebuilder/Grid.m
@@ -163,8 +163,7 @@ static const int GRID_COLUMNS = 10;
             if (currentCreature.livingNeighbors == 3) {
                 currentCreature.isAlive = YES;
                 numAlive++;
-            } else if (currentCreature.livingNeighbors <= 1 ||
-                       currentCreature.livingNeighbors >= 4) {
+            } else {
                 currentCreature.isAlive = NO;
             }
         }
